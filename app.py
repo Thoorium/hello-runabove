0354a8d481294f95de6f97458e35fa1bb5cd1f7d
Olivier Beaudoin <contact@olivierbeaudoin.net>
2015-02-15 11:24:58 -0500

    Improvements and details

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import bottle
-from bottle import route, template
+from bottle import template
 
-app = application = bottle.default_app()
+# Run bottle internal test server when invoked directly ie: non-uxsgi mode
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
+# Run bottle in application mode. Required in order to get the application working with uWSGI!
+else:
+    app = application = bottle.default_app()
 
-@route('/')
+@app.route('/')
 def index():
     return template('<h1>{{message}}</h1>', message='Hello Runabove!')
